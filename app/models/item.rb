45b8ac19bd02a1b2a2007d22edee90061d7ff457
kaisan-diary <75722248+kaisan-diary@users.noreply.github.com>
2021-01-08 18:22:46 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,16 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  with_options presence: true do  
+    validates :name
+    validates :explanation
+    validates :category_id
+    validates :condition_id
+    validates :shipping_burden_id
+    validates :prefecture_id
+    validates :shipping_date_id
+    validates :price
+    validates :image
+  end  
+
 end

89529b49dcf85fd87172d24df77026b33639e8fa
kaisan-diary <75722248+kaisan-diary@users.noreply.github.com>
2021-01-08 17:49:23 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
   belongs_to :user
   has_one_attached :image
+
 end

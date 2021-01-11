de54fe689687a1baa5a7f48c4eec702f6bb24f94
kaisan-diary <75722248+kaisan-diary@users.noreply.github.com>
2021-01-11 11:49:37 +0900

    出品した商品の情報をテーブルに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,10 +4,24 @@ class ItemsController < ApplicationController
 
 
   def index
-
+    @items = Item.all
   end
 
   def new
-    
+    @item = Item.new
   end
+
+  def create
+    @item = Item.create(item_params)
+     if @item.save
+       redirect_to root_path
+     else
+       render :new
+     end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :shipping_burden_id, :prefecture_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
+  end  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,20 +1,29 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_burden
+  belongs_to_active_hash :shipping_date
 
   belongs_to :user
   has_one_attached :image
 
-  with_options presence: true do  
+  with_options presence: true do 
+    validates :image 
     validates :name
     validates :explanation
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+  end  
+
+  validates :price, numericality: { only_integer: true, message: "Half-width number." }
+
+  with_options numericality: { other_than: 1, message: "Select" } do
     validates :category_id
     validates :condition_id
     validates :shipping_burden_id
     validates :prefecture_id
     validates :shipping_date_id
-    validates :price
-    validates :image
   end  
 
 end
